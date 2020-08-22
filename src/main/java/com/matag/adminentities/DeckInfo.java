a8c805ad15887650aa011a6bdba0f981c9bc703d
Antonio Alonzi <antonioalonzi85@gmail.com>
2020-08-22 10:09:14 +0100

    Admin should be generating decks... not the game.

--- src/main/java/com/matag/adminentities/DeckInfo.java
+++ src/main/java/com/matag/adminentities/DeckInfo.java
@@ -2,19 +2,18 @@ package com.matag.adminentities;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
-import com.matag.cards.properties.Color;
+import com.matag.cards.Card;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
-
-import java.util.Set;
 
 @Value
 @AllArgsConstructor
 @JsonDeserialize(builder = DeckInfo.DeckInfoBuilder.class)
 @Builder(toBuilder = true)
 public class DeckInfo {
-  Set<Color> randomColors;
+  List<Card> cards;
 
   @JsonPOJOBuilder(withPrefix = "")
   public static class DeckInfoBuilder {
