dda18e3a848cf3017c0bef0bc562c8b915cf206f
Leandro Doctors <ldoctors@gmail.com>
2021-05-08 23:23:02 -0300

    Organize imports

--- src/main/java/com/matag/adminentities/DeckInfo.java
+++ src/main/java/com/matag/adminentities/DeckInfo.java
@@ -1,9 +1,11 @@
 package com.matag.adminentities;
 
+import java.util.List;
+
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import com.matag.cards.Card;
-import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
